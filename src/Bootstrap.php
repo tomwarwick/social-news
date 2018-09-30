9e37aa3cd2ef9d4d13b1036ad9bc113e67c1b94d
Tom Warwick <tom.warwick@typo3.org>
2018-09-30 10:25:26 +0100

    Add Auryn dependancy injector package
    Chapter 3

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -42,13 +42,8 @@ switch ($routeInfo[0]) {
     case \FastRoute\Dispatcher::FOUND:
         [$controllerName, $method] = explode('#', $routeInfo[1]);
         $vars = $routeInfo[2];
-
-        $factory = new SocialNews\Framework\Rendering\TwigTemplateRendererFactory();
-
-        $templateRenderer = $factory->create();
-        $controller = new $controllerName($templateRenderer);
-
-//        $controller = new $controllerName;
+        $injector = include('Dependencies.php');
+        $controller = $injector->make($controllerName);
         $response = $controller->$method($request, $vars);
         break;
 }
