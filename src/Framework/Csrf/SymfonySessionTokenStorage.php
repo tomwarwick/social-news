23870dfe391dc91bf0fd5a632654b357f18d107b
Tom Warwick <tom.warwick@typo3.org>
2018-10-07 20:35:04 +0100

    Create token validator class

--- src/Framework/Csrf/SymfonySessionTokenStorage.php
+++ src/Framework/Csrf/SymfonySessionTokenStorage.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SocialNews\Framework\Csrf;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
--- src/Submission/Presentation/SubmissionController.php
+++ src/Submission/Presentation/SubmissionController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SocialNews\Submission\Presentation;
 
+use SocialNews\Framework\Csrf\StoredTokenValidator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use SocialNews\Framework\Rendering\TemplateRenderer;
@@ -11,10 +12,16 @@ use SocialNews\Framework\Rendering\TemplateRenderer;
 final class SubmissionController
 {
     private $templateRenderer;
+    private $storedTokenValidator;
 
-    public function __construct(TemplateRenderer $templateRenderer)
+    public function __construct
+    (
+        TemplateRenderer     $templateRenderer,
+        StoredTokenValidator $storedTokenValidator
+    )
     {
         $this->templateRenderer = $templateRenderer;
+        $this->storedTokenValidator = $storedTokenValidator;
     }
 
     public function show(): Response
