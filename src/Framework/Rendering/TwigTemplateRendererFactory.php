256c083939ad911c25c288ed7fe2b35d4637125b
Unknown <tom.warwick@typo3.org>
2018-10-08 20:25:43 +0100

    Update HTML templates to carry error messages

--- src/Framework/Rendering/TwigTemplateRendererFactory.php
+++ src/Framework/Rendering/TwigTemplateRendererFactory.php
@@ -4,24 +4,30 @@ declare(strict_types=1);
 
 namespace SocialNews\Framework\Rendering;
 
+use SocialNews\Framework\Csrf\StoredTokenReader;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 use Twig_Function;
 
-use SocialNews\Framework\Csrf\StoredTokenReader;
 
 final class TwigTemplateRendererFactory
 {
+    private $storedTokenReader;
     private $templateDirectory;
+    private $session;
 
     public function __construct
     (
         TemplateDirectory $templateDirectory,
-        StoredTokenReader $storedTokenReader
+        StoredTokenReader $storedTokenReader,
+        Session           $session
     )
     {
         $this->templateDirectory = $templateDirectory;
         $this->storedTokenReader = $storedTokenReader;
+        $this->session           = $session;
 
     }
 
@@ -36,6 +42,12 @@ final class TwigTemplateRendererFactory
             new Twig_Function('get_token', function (string $key): string {
                 $token = $this->storedTokenReader->read($key);
                 return $token->toString();
+            })
+        );
+
+        $twigEnvironment->addFunction(
+            new Twig_Function('get_flash_bag', function (): FlashBagInterface {
+                return $this->session->getFlashBag();
             })
         );
 
