2904d3c683639369bc32fe7f47de2b291a794fc9
Tom Warwick <tom.warwick@typo3.org>
2018-10-11 18:12:50 +0100

    Create dbal submission repository

---
+++ src/Submission/Infrastructure/DbalSubmissionRepository.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: TomWarwick
+     * Date: 11/10/2018
+     * Time: 17:57
+     */
+    declare(strict_types=1);
+
+
+    namespace SocialNews\Submission\Infrastructure;
+
+
+    class DbalSubmissionRepository
+    {
+
+    }
